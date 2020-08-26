11889fe0ea71bf939a64bc3b9c5523c48b0618fe
dpankov91 <dpankov91@gmail.com>
2020-08-26 11:48:07 +0300

    Solution created

---
+++ PetShopApp2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PetShopApp2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
